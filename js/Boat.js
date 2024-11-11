c109d3557a2054d766cf91d31453d764220e7a1d
Tátila Bianca Durão Fernandes <91564423+tatidurao@users.noreply.github.com>
2024-11-11 13:51:17 -0300

    Boat.js

--- js/Boat.js
+++ js/Boat.js
@@ -1,7 +1,6 @@
 class Boat {
-  constructor(x, y, width, height, boatPos, boatAnimation) {
+  constructor(x, y, width, height, boatPos) {
   
-    this.animation = boatAnimation;
     this.speed = 0.05;
     this.body = Bodies.rectangle(x, y, width, height);
     this.width = width;
@@ -12,9 +11,7 @@ class Boat {
 
     World.add(world, this.body);
   }
-  animate() {
-    this.speed += 0.05;
-  }
+  
 
   remove(index) {
     setTimeout(() => {
@@ -26,13 +23,13 @@ class Boat {
   display() {
     var angle = this.body.angle;
     var pos = this.body.position;
-    var index = floor(this.speed % this.animation.length);
+   
 
     push();
     translate(pos.x, pos.y);
     rotate(angle);
     imageMode(CENTER);
-    image(this.animation[index], 0, this.boatPosition, this.width, this.height);
+    image(this.image, 0, this.boatPosition, this.width, this.height);
     pop();
   }
 }
